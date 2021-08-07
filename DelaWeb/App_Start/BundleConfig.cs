94e3456ebc71687be27a75fb0bcce3f32b190d4a
Emmanuel´s Lap <segcro7@gmail.com>
2021-08-07 18:31:27 -0500

    moving images

--- DelaWeb/App_Start/BundleConfig.cs
+++ DelaWeb/App_Start/BundleConfig.cs
@@ -18,8 +18,8 @@ namespace DelaWeb
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
